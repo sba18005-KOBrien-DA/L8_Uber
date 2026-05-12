308ed9eaa1a3d5a5231a990c71b63945a3451139
sba18005-KOBrien-DA <sba18005@student.cct.ie>
2026-05-12 19:02:36 +0100

    5

--- uber.py
+++ uber.py
@@ -26,3 +26,9 @@ data_load_state.text("Loading data Done!")
 st.subheader("Raw Data")
 st.write(data)
 
+
+st.subheader("Num Pickups per hour")
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+st.barchart(hist_values)
+
+
